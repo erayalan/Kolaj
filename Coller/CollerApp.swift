71d5e4daa37773685c998cb0c044bd7427b72f6e
Eray Alan <erayalan@gmail.com>
2026-01-12 16:55:55 -0800

    Initial Commit

---
+++ Coller/CollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollerApp.swift
+//  Coller
+//
+//  Created by Eray Alan on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coller
+//
+//  Created by Eray Alan on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
